5f1ca4d21e7bf453258f988d06e65b2f72c66077
geoffrey woodcock <theatomproject@gmail.com>
2019-03-27 15:57:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // Write your code in this file!
 
-function scuberGreetingForFeet(someValue){
-  let result, result2
-  if (someValue <= 400) {
-    result = 'This one is on me!';
-    return result
-  } 
-  elsif (someValue > 2000){
-    result2 = 'I will gladly take your thirty bucks.';
-    return result2
+function scuberGreetingForFeet (feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (1999 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
   }
 }
   
