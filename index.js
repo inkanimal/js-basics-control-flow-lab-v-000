63d1e36bb6f76fc6273cf3aac4a777777163de61
geoffrey woodcock <theatomproject@gmail.com>
2019-03-27 15:51:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ function scuberGreetingForFeet(someValue){
   let result1, result2, result3
   if (someValue <= 400) {
     result1 = 'This one is on me!';
-  } elsif (someValue > 2000  )
-    result2 = 
-}
+  } 
+  elsif (someValue > 2000  ){
+    result2 = 'I will gladly take your thirty bucks.'
+  }
+  eslif (someValue > 2501){
+    result3 = 
+  }
   
      
