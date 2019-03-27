e198372cf5349caf86102ff732a74cfaf7bff5c3
geoffrey woodcock <theatomproject@gmail.com>
2019-03-27 16:16:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function ternaryCheckCity(city) {
   return city === "NYC" ?  'Ok, sounds good.' : "No go."}
   
 function  switchOnCharmFromTip(tip){
-   tip = 'generous'
+   
    switch (tip)
       case 'generous';
       return "Thank you so much.";
