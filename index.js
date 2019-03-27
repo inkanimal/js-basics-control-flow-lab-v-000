1670c2930bfdb57dde696f3bfcd600d13fb10415
geoffrey woodcock <theatomproject@gmail.com>
2019-03-27 15:51:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function scuberGreetingForFeet(someValue){
     result2 = 'I will gladly take your thirty bucks.'
   }
   eslif (someValue > 2501){
-    result3 = 
+    result3 = 'No can do.'
   }
+}
   
      
