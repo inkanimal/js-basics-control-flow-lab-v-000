8d24ae531285d070c66ce15d067060c272128b21
geoffrey woodcock <theatomproject@gmail.com>
2019-03-27 15:54:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,20 +7,6 @@ function scuberGreetingForFeet(someValue){
   } 
 }
   
-  function scuberGreetingForFeet(someValue){
-  let result
-  if (someValue > 2000) {
-    result = 'I will gladly take your thirty bucks.';
-    return result
-  } 
-  }
-  
-  function scuberGreetingForFeet(someValue){
-  let result
-  eslif (someValue > 2501){
-    result3 = 'No can do.'
-    return result
-  }
-}
+ 
   
      
