fdde40df6291ed5cdeb118dcf43a7ed43282ddaf
geoffrey woodcock <theatomproject@gmail.com>
2019-03-27 16:15:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,15 +18,13 @@ function  switchOnCharmFromTip(tip){
    switch (tip)
       case 'generous';
       return "Thank you so much.";
-      break;
-      
+     
       case 'not as generous';
       return 'Thank you';
-      break;
-      
+    
       case 'thanks for everything';
       return "Bye.";
-      break}
+     
       
       
       
